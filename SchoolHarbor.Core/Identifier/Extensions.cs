b714e073a2c150d1175dedacb98c9bb56f27144d
AmsSchoolsDeveloper <AmsSchoolsDeveloper@amsschools.org>
2024-03-05 07:40:24 -0700

    referenceidexceptions

---
+++ SchoolHarbor.Core/Identifier/Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace SchoolHarbor.Core.Identifier;
+
+public static class Extensions
+{
+    public static int ExtractNumericValue(this IReferenceId referenceId)
+    {
+        if(int.TryParse(referenceId.Value, out var result))
+        {
+               return result;
+        }
+
+        throw new InvalidDataException($"ReferenceId is not valid int. Value: {referenceId.Value}");
+    }
+
+    public static bool IsSchoolHarborDbId(this IReferenceId referenceId)
+    {
+        return referenceId.Kind == ReferenceIdKind.Database &&
+               referenceId.SourceKind == ReferenceIdSourceKind.SchoolHarbor;
+    }
+}
